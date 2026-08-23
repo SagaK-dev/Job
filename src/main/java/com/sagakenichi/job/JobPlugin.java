9c153b2adfa2316bf4767dc8d94723b8863b26f5
K.Saga <sagakatsuyuki.555@gmail.com>
2026-08-23 10:58:42 +0900

    Wire Vault economy and runtime configuration

---
+++ src/main/java/com/sagakenichi/job/JobPlugin.java
@@ -0,0 +1,74 @@
+package com.sagakenichi.job;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JobPlugin extends JavaPlugin {
+
+    private Economy economy;
+    private BeginnerService beginners;
+    private RewardService rewards;
+    private PlacedBlockTracker placedBlocks;
+    private JobListener listener;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        if (!setupEconomy()) {
+            getLogger().severe("Vault and a Vault-compatible economy provider are required. Disabling Job.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        beginners = new BeginnerService(
+                getConfig().getLong("beginner.duration-hours", 72L),
+                getConfig().getInt("beginner.multiplier", 3));
+        rewards = new RewardService(this, economy, beginners,
+                getConfig().getBoolean("messages.payout", true));
+        placedBlocks = new PlacedBlockTracker(this,
+                getConfig().getBoolean("anti-abuse.ignore-player-placed-blocks", true));
+        RewardCatalog catalog = new RewardCatalog();
+        listener = new JobListener(catalog, rewards, placedBlocks, beginners,
+                getConfig().getBoolean("messages.beginner-join", true));
+
+        getServer().getPluginManager().registerEvents(listener, this);
+
+        PluginCommand command = getCommand("job");
+        if (command == null) {
+            throw new IllegalStateException("Command 'job' is missing from plugin.yml");
+        }
+        JobCommand executor = new JobCommand(this, beginners, rewards);
+        command.setExecutor(executor);
+        command.setTabCompleter(executor);
+
+        getLogger().info("Job 1.0.0 enabled. Beginner rewards are multiplied by "
+                + beginners.beginnerMultiplier() + " for the first "
+                + getConfig().getLong("beginner.duration-hours", 72L) + " hours.");
+    }
+
+    void reloadRuntimeConfig() {
+        reloadConfig();
+        beginners.reload(
+                getConfig().getLong("beginner.duration-hours", 72L),
+                getConfig().getInt("beginner.multiplier", 3));
+        rewards.setPayoutMessages(getConfig().getBoolean("messages.payout", true));
+        placedBlocks.setEnabled(getConfig().getBoolean("anti-abuse.ignore-player-placed-blocks", true));
+        listener.setBeginnerJoinMessage(getConfig().getBoolean("messages.beginner-join", true));
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> registration =
+                getServer().getServicesManager().getRegistration(Economy.class);
+        if (registration == null) {
+            return false;
+        }
+        economy = registration.getProvider();
+        return economy != null;
+    }
+}
